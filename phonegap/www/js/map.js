fed54c2541b47dedf04b0f89a973d039dd68aa90
Brenda Jin <brenda.jin@macys.com>
2013-06-23 00:56:00 -0700

    map working

--- phonegap/www/js/map.js
+++ phonegap/www/js/map.js
@@ -1,18 +1,73 @@
-var map = L.map('map').setView([37.7750, -122.4183], 13);
+this.App = this.App || {};
 
+var Map = L.map('map');
+
+// L.map('map').setView([37.7750, -122.4183], 13);
 
 var cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-	cmUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/{styleId}/256/{z}/{x}/{y}.png';
+	cmUrl = 'http://{s}.tile.cloudmade.com/51c0f5a1659d402ab930cdb5fa2a0f73/{styleId}/256/{z}/{x}/{y}.png';
 
-var minimal   = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr}),
-	midnight  = L.tileLayer(cmUrl, {styleId: 999,   attribution: cmAttr}),
-	motorways = L.tileLayer(cmUrl, {styleId: 46561, attribution: cmAttr});
+var minimal   = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr});
 
-minimal.addTo(map);
+minimal.addTo(Map);
+
+
+function onLocationFound(e) {
+    map.locate({setView: true, maxZoom: 13});
+}
+
+map.on('locationfound', onLocationFound);
+
+
 
 var circle = L.circle([
 37.78724,-122.399034], 100, {
     color: '#851c16',
     fillColor: '#851c16',
     fillOpacity: 0.75
-}).addTo(map);
+}).addTo(Map);
+
+var circle = L.circle([
+37.789831,-122.424134], 50, {
+    color: '#851c16',
+    fillColor: '#851c16',
+    fillOpacity: 0.75
+}).addTo(Map);
+
+Map.makeCircle = function(size) {
+	var circle = L.circle([
+		37.78724,-122.399034], size*10, {
+	    color: '#851c16',
+	    fillColor: '#851c16',
+	    fillOpacity: 0.75
+	}).addTo(Map);
+}
+
+Map.fetch = function() {
+	var time = Date.now() - 86400000;
+	var ql = "select * where created > " + time;
+
+	var options = { 
+	   method:'GET', 
+	   endpoint:'peps',
+	   qs: {
+	   	'ql':ql,
+	   	'limit':1000
+	   }
+	}; 
+
+	var diam='string';
+
+	client.request(options, function (err, data) { 
+		if (err) { //error 
+			console.log(err);
+		} else { //success — data will contain raw results from API call 
+			console.log(data.entities.length);
+			Map.makeCircle(data.entities.length);
+		} 
+	});	
+};
+
+$(Map.makeCircle(10));
+
+window.setInterval(Map.fetch, 1000);
